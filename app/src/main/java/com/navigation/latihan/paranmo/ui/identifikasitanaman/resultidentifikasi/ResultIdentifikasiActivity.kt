5bed6fb335fa5e2d36e196a7fd1fcbf3143a525c
Givar Elva Rizky H <61646945+givarelvarizky@users.noreply.github.com>
2022-06-02 14:24:59 +0700

    paranmo

--- app/src/main/java/com/navigation/latihan/paranmo/ui/identifikasitanaman/resultidentifikasi/ResultIdentifikasiActivity.kt
+++ app/src/main/java/com/navigation/latihan/paranmo/ui/identifikasitanaman/resultidentifikasi/ResultIdentifikasiActivity.kt
@@ -1,13 +1,82 @@
 package com.navigation.latihan.paranmo.ui.identifikasitanaman.resultidentifikasi
 
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.navigation.latihan.paranmo.R
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.navigation.latihan.paranmo.databinding.ActivityResultIdentifikasiBinding
+import com.navigation.latihan.paranmo.ml.ParanmoFinal
+import org.tensorflow.lite.support.image.TensorImage
 
 class ResultIdentifikasiActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityResultIdentifikasiBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(
-            R.layout.activity_result_identifikasi)
+        binding = ActivityResultIdentifikasiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindingButton()
     }
+
+    private fun bindingButton(){
+
+        binding.fabCam.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_GRANTED){
+                pictPreview.launch(null)
+            }else{
+                requestPermission.launch(android.Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            permission ->
+
+        if (permission){
+            pictPreview.launch(null)
+        } else{
+            Toast.makeText(this, "Izin ditolak !! Coba lagi", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val pictPreview = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){
+            bitmap ->
+
+        if(bitmap != null){
+            binding.photo.setImageBitmap(bitmap)
+            binding.cardIdentification.visibility = View.VISIBLE
+            binding.description.visibility = View.GONE
+            binding.useCard.visibility = View.GONE
+            output(bitmap)
+        }
+    }
+
+    private fun output(bitmap: Bitmap) {
+
+        val modelParanmoFinal = ParanmoFinal.newInstance(this)
+
+        val imageBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val tensorImage = TensorImage.fromBitmap(imageBitmap)
+
+        val outputs = modelParanmoFinal.process(tensorImage)
+            .probabilityAsCategoryList.apply {
+                sortByDescending { it.score }
+            }
+
+        val probabilityOutput = outputs[0]
+
+
+        binding.plant.text = probabilityOutput.label
+
+        Log.i("TAG", "output : $probabilityOutput")
+    }
+
 }
