490570b9851eaf48541f85738bb73c5d0e83914e
Givar Elva Rizky H <61646945+givarelvarizky@users.noreply.github.com>
2022-06-08 02:39:04 +0700

    paranmo

--- app/src/main/java/com/navigation/latihan/paranmo/ui/home/favorit/FavoritActivity.kt
+++ app/src/main/java/com/navigation/latihan/paranmo/ui/home/favorit/FavoritActivity.kt
@@ -3,12 +3,10 @@ package com.navigation.latihan.paranmo.ui.home.favorit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.navigation.latihan.paranmo.R
 import com.navigation.latihan.paranmo.databinding.ActivityFavoritBinding
 import com.navigation.latihan.paranmo.ui.MainActivity
-import com.navigation.latihan.paranmo.ui.home.HomeFragment
-import com.navigation.latihan.paranmo.ui.identifikasitanaman.cameraidentifikasi.CameraIdentifikasiActivity
 
+@Suppress("UNUSED_EXPRESSION")
 class FavoritActivity : AppCompatActivity() {
 
     private lateinit var  binding: ActivityFavoritBinding
@@ -18,10 +16,11 @@ class FavoritActivity : AppCompatActivity() {
         binding = ActivityFavoritBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.backFavorit.setOnClickListener{
+        binding.backFavorit.setOnClickListener {
             val view = Intent(this@FavoritActivity, MainActivity::class.java)
             startActivity(view)
         }
 
     }
+
 }
